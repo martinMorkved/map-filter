48c8540e522cd916c294ff31566e7c6a7980ae62
martinMorkved <38659462+martinMorkved@users.noreply.github.com>
2020-10-26 08:52:59 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      var numbers = [3, 56, 2, 48, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+
+//const newNumbers = numbers.map(function (x) {
+//  return x * 2;
+//});
+
+//console.log(newNumbers);
+
+//Filter - Create a new array by keeping the items that return true.
+
+//const newNumbers = numbers.filter(function (num) {
+//  return num > 10;
+//});
+
+//console.log(newNumbers);
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+//var newNumber = numbers.reduce(function (accumulator, currentNumber) {
+//  return accumulator + currentNumber;
+//});
+
+//var newNumber = 0;
+//numbers.forEach(function (currentNumber) {
+//  newNumber += currentNumber;
+//});
+
+//console.log(newNumber);
+
+//Find - find the first item that matches from an array.
+
+const newNumber = numbers.findIndex(function (num) {
+  return num > 10;
+});
+
+console.log(newNumber);
+
+//FindIndex - find the index of the first item that matches.
+
+    </div>
+  );
+}
+
+export default App;

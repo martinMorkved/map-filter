2886ca81eb978e7f8b4dc5a8f2a0f696c62a1225
martinMorkved <38659462+martinMorkved@users.noreply.github.com>
2020-10-29 08:12:33 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,39 +8,26 @@ function App() {
 //Map -Create a new array by doing something with each item in an array.
 
 //const newNumbers = numbers.map(function (x) {
-//  return x * 2;
-//});
+//var numbers = [3, 56, 2, 48, 5];
+
+//const newNumbers = numbers.map((x) => x * x);
 
 //console.log(newNumbers);
 
-//Filter - Create a new array by keeping the items that return true.
+////Map -Create a new array by doing something with each item in an array.
+//const newNumbers = numbers.map((x) => x * 2);
 
-//const newNumbers = numbers.filter(function (num) {
-//  return num > 10;
-//});
-
-//console.log(newNumbers);
+//////Filter - Create a new array by keeping the items that return true.
+//const newNumbers = numbers.filter((num) => num < 10);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
+// var newNumber = numbers.reduce( (accumulator, currentNumber) => accumulator + currentNumber);
 
-//var newNumber = numbers.reduce(function (accumulator, currentNumber) {
-//  return accumulator + currentNumber;
-//});
+////Find - find the first item that matches from an array.
+//const newNumber = numbers.find((num) => num > 10);
 
-//var newNumber = 0;
-//numbers.forEach(function (currentNumber) {
-//  newNumber += currentNumber;
-//});
-
-//console.log(newNumber);
-
-//Find - find the first item that matches from an array.
-
-//const newNumber = numbers.findIndex(function (num) {
-//  return num > 10;
-//});
-
-//console.log(newNumber);
+////FindIndex - find the index of the first item that matches.
+//const newNumber = numbers.findIndex((num) => num > 10);
 
 //FindIndex - find the index of the first item that matches.
 
